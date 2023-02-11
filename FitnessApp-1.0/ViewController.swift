0278fde09470601e0ac1fecf432525c2c5f862a2
Vadim <91338647+pticagamaun@users.noreply.github.com>
2023-02-11 14:21:26 +0300

    Initial commit

---
+++ FitnessApp-1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp-1.0
+//
+//  Created by Vadim on 11.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
