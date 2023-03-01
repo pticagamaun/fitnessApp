f5a0bc61ae4a08ab41ad962afa3fb07c4c88396d
Vadim <91338647+pticagamaun@users.noreply.github.com>
2023-03-01 23:51:18 +0300

    Add func toMinutesAndSeconds

--- FitnessApp-1.0/Extension/Int + Extenstion.swift
+++ FitnessApp-1.0/Extension/Int + Extenstion.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension Int {
+    
+    func toMinutesAndSeconds() -> String {
+        switch self {
+        case ..<60: return "\(self) sec"
+        case let sec where sec % 60 == 0: return "\(self / 60) min"
+        default: return "\(self / 60) min \(self % 60) sec"
+        }
+    }
+}
