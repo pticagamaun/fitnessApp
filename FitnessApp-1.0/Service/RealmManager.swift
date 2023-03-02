3265f814041e1cef809c7012b089067fa647459f
Vadim <91338647+pticagamaun@users.noreply.github.com>
2023-03-03 00:45:13 +0300

    Add Service layer to Realm on base Singleton

--- FitnessApp-1.0/Service/RealmManager.swift
+++ FitnessApp-1.0/Service/RealmManager.swift
@@ -5,4 +5,70 @@
 //  Created by Vadim on 02.03.2023.
 //
 
-import Foundation
+import RealmSwift
+
+class RealmManager {
+    
+    static let shared = RealmManager()
+    private init() {}
+    private let realm = try! Realm()
+    
+    func getWorkoutModelResults() -> Results<WorkoutModel> {
+        realm.objects(WorkoutModel.self)
+    }
+    
+    func save(_ model: WorkoutModel) {
+        do {
+            try realm.write {
+                realm.add(model)
+            }
+        } catch let error {
+            print("Error saving WorkoutModel: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete(_ model: WorkoutModel) {
+        do {
+            try realm.write {
+                realm.delete(model)
+            }
+        } catch let error {
+            print("Error clearing WorkoutModel: \(error.localizedDescription)")
+        }
+    }
+    
+    func updateRepsSetsWorkoutModel(model: WorkoutModel, sets: Int, reps: Int) {
+        do {
+            try realm.write{
+                model.workoutSets = sets
+                model.workoutReps = reps
+            }
+        } catch let error {
+            print("Error clearing WorkoutModel: \(error.localizedDescription)")
+        }
+    }
+    
+    func updateTimerSetsWorkoutModel(model: WorkoutModel, sets: Int, timer: Int) {
+        do {
+            try realm.write {
+                model.workoutSets = sets
+                model.workoutTimer = timer
+            }
+        } catch let error {
+            print("Error clearing WorkoutModel: \(error.localizedDescription)")
+        }
+    }
+    
+    func updateStatusWorkoutModel(model: WorkoutModel) {
+        do {
+            try realm.write {
+                model.workoutStatus = true
+            }
+        } catch let error {
+            print("Error clearing WorkoutModel: \(error.localizedDescription)")
+        }
+    }
+}
+
+
+
